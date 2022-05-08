92438b89265102826825dc9aa6d0fcc7a2174c95
sabiha-wasema <sabihawasemaa1244@gmail.com>
2022-05-08 13:58:47 +0600

    addproduct update

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ async function run() {
             const product = await productCollection.findOne(query);
             res.send(product);
         });
+
+        // post
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        });
     }
     finally {
 
