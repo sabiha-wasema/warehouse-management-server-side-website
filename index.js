3e8d4d5bf53fd6d442e692a033ac15886a76748c
sabiha-wasema <sabihawasemaa1244@gmail.com>
2022-05-08 13:55:31 +0600

    route added

--- index.js
+++ index.js
@@ -15,7 +15,25 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8v7rf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('gaucheFruitCenter').collection('product');
 
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running Gauche Server');
