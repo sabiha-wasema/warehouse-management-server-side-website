3d11e4564e2e3cd4b6c6de439a49ad12dd00e43e
sabiha-wasema <sabihawasemaa1244@gmail.com>
2022-05-09 16:16:06 +0600

    token added

--- index.js
+++ index.js
@@ -11,8 +11,21 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8v7rf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
